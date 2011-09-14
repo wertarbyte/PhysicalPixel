b2bb193fe6d05aeec0a7fc14d57c96e5d118b588
Stefan Tomanek <stefan@pico.ruhr.de>
2011-09-14 19:36:51 +0200

    calculate average color (slow!)

--- software/pixeltrack.c
+++ software/pixeltrack.c
@@ -12,12 +12,35 @@
 #include <stdint.h>
 #include "hid.h"
 
-void get_pixel_color(Display *d, int x, int y, XColor *c) {
+struct rgb_color {
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;
+};
+
+void get_pixel_color(Display *d, int x, int y, struct rgb_color *c, int radius) {
 	XImage *img;
-	img = XGetImage(d, RootWindow (d, DefaultScreen (d)), x, y, 1, 1, AllPlanes, XYPixmap);
-	c->pixel = XGetPixel(img, 0, 0);
+	img = XGetImage(d, RootWindow (d, DefaultScreen (d)), x-radius, y-radius, 2*radius, 2*radius, AllPlanes, XYPixmap);
+	XColor xc;
+	/* calculate average color */
+	uint8_t ix, iy;
+	double r = 0;
+	double g = 0;
+	double b = 0;
+	int pixels = (img->width*img->height);
+	for (ix=0; ix<img->width; ix++) {
+		for (iy=0; iy<img->height; iy++) {
+			xc.pixel = XGetPixel(img, ix, iy);
+			XQueryColor(d, DefaultColormap(d, DefaultScreen(d)), &xc);
+			r += 1.0*xc.red/pixels;
+			g += 1.0*xc.green/pixels;
+			b += 1.0*xc.blue/pixels;
+		}
+	}
+	c->red = (xc.red/256);
+	c->green = (xc.green/256);
+	c->blue = (xc.blue/256);
 	XFree(img);
-	XQueryColor(d, DefaultColormap(d, DefaultScreen(d)), c);
 }
 
 void get_cursor_position(Display *d, int *x, int *y) {
@@ -51,21 +74,21 @@ int main(int argc, char *argv[]) {
 	int y = -1;
 	int old_x = -1;
 	int old_y = -1;
-	XColor color;
+	struct rgb_color color;
 	uint8_t buf[64];
 	while(1) {
 		get_cursor_position(d, &x, &y);
 		if (x != old_x || y != old_y) {
-			get_pixel_color(d, x, y, &color);
-			printf("%d/%d\t(%d/%d/%d)\n", x, y, color.red/256, color.green/256, color.blue/256);
+			get_pixel_color(d, x, y, &color, 1);
+			printf("%d/%d\t(%d/%d/%d)\n", x, y, color.red, color.green, color.blue);
 			if (usb_present) {
 				uint8_t i;
 				for (i=0; i<64; i++) {
 					buf[i] = 0;
 				}
-				buf[0] = color.red/256;
-				buf[1] = color.green/256;
-				buf[2] = color.blue/256;
+				buf[0] = color.red;
+				buf[1] = color.green;
+				buf[2] = color.blue;
 				rawhid_send(0, buf, 64, 100);
 			}
 			old_x = x;
